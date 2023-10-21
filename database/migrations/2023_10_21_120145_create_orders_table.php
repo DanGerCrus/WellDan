4e1ee85d4f157d2bcfa0359794a18aee98745c47
MrCrross <stanislon1@gmail.com>
2023-10-22 02:39:42 +0800

    first commit

---
+++ database/migrations/2023_10_21_120145_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date_order');
+            $table->unsignedBigInteger('status_id');
+            $table->unsignedBigInteger('creator_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('creator_id')->on('users')->references('id');
+            $table->foreign('status_id')->on('statuses')->references('id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
